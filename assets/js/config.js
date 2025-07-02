d25bed134c07bb8534e53336c3d598374f68b622
Wordy <wordymarte848@gmail.com>
2025-07-02 19:11:34 -0400

    actualizacion de congf

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 // Configuración global de la aplicación
 const CONFIG = {
-    API_BASE_URL: 'http://127.0.0.1:8000/',
+    API_BASE_URL: 'http://127.0.0.1:8000',
     ENDPOINTS: {
         LOGIN: '/internal/auth/login',
         REGISTER: '/internal/auth/registrar'
